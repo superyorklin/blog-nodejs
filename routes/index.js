ed52f0cbbbc0ab32e81b7cff79a14991032eeb92
linyz <linyz@uyunsoft.cn>
2017-07-18 17:41:15 +0800

    to be continue

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ module.exports = function(app){
   app.get('/',function(req,res){
     res.render('index', { title: 'Express' });
   });
+
+  /*
+  * 管理员访问，上传md文件转换而成的html
+  * 文章数据保存入数据库
+  **/
   app.post('/admin',multipartMiddleware,function(req,res){
     //console.log(req.body);
     //console.log(req.files);
@@ -18,6 +23,44 @@ module.exports = function(app){
     }else{
       console.log(result[1]);
       res.send("done");
+    }
+  });
+
+  /*
+  * 获取文章信息
+  *
+  * */
+  app.get('/allArtical',function (req,res) {
+    var limit = + req.query.limit;
+    var articals = [{
+        id: "one",
+        title: "react-router学习--york",
+        time: "2017-5-20"
+    },{
+        id: "two",
+        title: "TCP协议简介",
+        time: "2017-5-21"
+    },{
+        id: "three",
+        title: "rxjs学习笔记",
+        time: "2017-5-22"
+    },{
+        id: "four",
+        title: "markdown",
+        time: "2017-5-23"
+    },{
+        id: "five",
+        title: "Server-Sent Events 教程",
+        time: "2017-5-25"
+    },{
+        id: "six",
+        title: "express总结",
+        time: "2017-5-29"
+    }];
+    if(!limit){
+      res.send(articals.slice(0,limit+1));
+    }else {
+      res.send(articals);
     }
   })
 }
